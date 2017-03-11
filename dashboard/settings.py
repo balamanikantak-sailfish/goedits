4e3ef3739326f9e0dac3f2773a837510232333e4
Bala Manikanta Koorakula <balamanikantak-sailfish@users.noreply.github.com>
2017-03-11 18:59:37 +0530

    Update settings.py

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 
 # send grid
 # please replace "<sendgrid_username>" and "<sendgrid_password>" with your SendGrid credentials
-SENDGRID_USERNAME = 'manikanta';
+SENDGRID_USERNAME = 'kbmanikanta90';
 SENDGRID_PASSWORD = 'bala13422';
 
 
